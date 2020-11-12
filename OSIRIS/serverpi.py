0f610f1e1b573d1f26800f9397e2b6f9261b35f7
monishkumar-sivakumar <63323924+monishkumar-sivakumar@users.noreply.github.com>
2020-11-11 20:21:13 -0500

    [Monishkumar Sivakumar] Feat: Changed Humidity Value

--- OSIRIS/serverpi.py
+++ OSIRIS/serverpi.py
@@ -18,7 +18,7 @@ class read(unittest.TestCase):
         pressure = get_data['feeds']['pressure']
         
         self.assertTrue(temperature < 50, msg="There is a possibility of a fire")
-        self.assertTrue(humidity < 5, msg="The air is dry, with a possibility of a fire")
+        self.assertTrue(humidity < 35, msg="The air is dry, with a possibility of a fire")
         self.assertTrue(pressure < 90 || pressure > 120, msg="The pressure is not in normal ranges. There could be a leaky roof or air is entering the room. ")
     
         
