bf2fa27036b78aca7e08d5a498eabf55fa6b526c
TimothyKnowles <71571216+TimothyKnowles@users.noreply.github.com>
2020-12-08 17:10:40 -0500

    Delete serverpi3.py

--- serverpi/serverpi3.py
+++
@@ -1,9 +0,0 @@
-import serverpi_humidity
-import serverpi_temperature
-import serverpi_pressure
-
-if __name__ == "__main__":
-    
-    serverpi_humidity.pollHumidity()
-    serverpi_temperature.pollTemperature()
-    serverpi_pressure.pollPressure()
